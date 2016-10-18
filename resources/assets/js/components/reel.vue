afb87b510a8358bb9552ced7c3d3d1f6b5e6b1a8
Ricki Moore <rickimoore@Rickis-MacBook-Air.local>
2016-10-18 21:40:57 +0200

    changed fallback to correct public  path

--- resources/assets/js/components/reel.vue
+++ resources/assets/js/components/reel.vue
@@ -6,7 +6,7 @@
                     <source :src="reel[0].path" type="video/mp4" />
                 </template>
                 <template v-else>
-                    <source src="/Users/rickimoore/Laravel/FABIANA/public/video/promo.mp4" type="video/mp4" />
+                    <source src="/public/video/promo01.mp4" type="video/mp4" />
                 </template>
                 <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
             </video>
