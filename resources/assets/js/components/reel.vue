75761993fa36fe402b2f9b14ab74bc650bb042e0
Ricki Moore <rickimoore@Rickis-MacBook-Air.local>
2016-10-18 21:43:48 +0200

    deleted public to the src path

--- resources/assets/js/components/reel.vue
+++ resources/assets/js/components/reel.vue
@@ -6,7 +6,7 @@
                     <source :src="reel[0].path" type="video/mp4" />
                 </template>
                 <template v-else>
-                    <source src="/public/video/promo01.mp4" type="video/mp4" />
+                    <source src="/video/promo01.mp4" type="video/mp4" />
                 </template>
                 <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
             </video>
